ec9b9ab019df4ed01c13ef2922cea060e769b974
TimmKidd <zheleznovtema@yandex.ru>
2026-04-25 16:39:10 +0300

    Repository sanitation after phase 5 checkpoint

--- _archive/legacy_code/legacy_findex_bot/handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .start import router as start_router
-from .moderation import router as moderation_router
-
-__all__ = ["start_router", "moderation_router"]
--- _archive/worktree_sanitation_20260420_125048/findex_bot/states/alerts.py
+++
@@ -1,8 +0,0 @@
-# findex_bot/states/alerts.py
-from aiogram.fsm.state import StatesGroup, State
-
-
-class AlertsForm(StatesGroup):
-    choosing_target = State()   # кого ловим
-    position = State()          # ключевые слова по должности
-    location = State()          # ключевые слова по локации
